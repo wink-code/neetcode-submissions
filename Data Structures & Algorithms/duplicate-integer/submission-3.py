d32a44e8105a4ab980cd526e0060a23cf495d733
johnwink <johnwink1998@gmail.com>
2026-05-05 12:13:40 +0800

    Add: duplicate-integer - submission-3

---
+++ Data Structures & Algorithms/duplicate-integer/submission-3.py
@@ -0,0 +1,10 @@
+class Solution:
+    def hasDuplicate(self, nums: List[int]) -> bool:
+        def value_greater_than_2(counter_dict: dict[int, int])->bool:
+            return any(value>=2 for value in counter_dict.values())
+        counter = {}
+        for num in nums:
+            counter[num] = counter.get(num, 0) + 1
+            if duplicated:=value_greater_than_2(counter):
+                return duplicated
+        return False
